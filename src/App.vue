ab6bed6ecec7471fc08cc6f1503c08a624c1a047
Sina Andac Tan <sina.andac@ar-lx0299.ar.int>
2023-08-03 16:04:49 +0200

    Fix const issue

--- src/App.vue
+++ src/App.vue
@@ -9,91 +9,52 @@ import { loadObject } from './loader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { useLoader } from './composables/useLoader';
 import URDFLoader, { type URDFRobot } from 'urdf-loader';
-import { join } from 'path';
 
 const sceneBackground = new THREE.Color('skyblue');
 
 const width = window.innerWidth;
 const height = window.innerHeight;
+const counter = 0;
 
-const handleMeshChange = (mesh: THREE.Mesh | URDFRobot, delta: number): void => {
-  const radiansLimit: {[key: string]: number} = {
-    "HP": 20,
-    "KP": 20,
-    "AP": 20
+const radiansLimit: {[key: string]: number} = {
+    "HP": THREE.MathUtils.degToRad(30),
+    "KP": THREE.MathUtils.degToRad(40),
+    "AP": THREE.MathUtils.degToRad(40)
   };
-  const radiansPerSecond = THREE.MathUtils.degToRad(120);
+
   const radiansPerSecondJoints: {[key: string]: number} = {
     "HP": THREE.MathUtils.degToRad(10),
     "KP": THREE.MathUtils.degToRad(10),
     "AP": THREE.MathUtils.degToRad(10)
   };
 
+const handleMeshChange = (mesh: THREE.Mesh | URDFRobot, delta: number): void => {
+
   const _calculateNextJointValue = (oldVal: number, delta: number, degree: number): number => {
     return oldVal + delta * degree;
   }
 
   const _setJointValue = (robot: URDFRobot, jointKey: string, delta: number, degree: number) => {
-    const jointValue = _calculateNextJointValue(robot.joints[jointKey].jointValue[0] as number, delta, degree);
-    if (jointKey.startsWith("HP")) {
-      console.log(Math.abs(THREE.MathUtils.radToDeg(jointValue)) >= Math.abs((radiansLimit[jointKey.slice(0, -1)])));
+    let jointValue = _calculateNextJointValue(robot.joints[jointKey].jointValue[0] as number, delta, degree);
+    // if (jointKey.startsWith("HP")) {
+    //   console.log(Math.abs(THREE.MathUtils.radToDeg(jointValue)) >= Math.abs((radiansLimit[jointKey.slice(0, -1)])));
+    // }
+    const jointGroupKey = jointKey.slice(0, -1);
+    if (Math.abs(jointValue) > Math.abs(radiansLimit[jointGroupKey])) {
+      jointValue = radiansLimit[jointGroupKey];
+      radiansLimit[jointGroupKey] = -radiansLimit[jointGroupKey];
+      radiansPerSecondJoints[jointGroupKey] = -radiansPerSecondJoints[jointGroupKey];
     }
-    if (Math.abs(THREE.MathUtils.radToDeg(jointValue)) >= Math.abs((radiansLimit[jointKey.slice(0, -1)]))) {
-      const _key = jointKey.slice(0, -1);
-      radiansLimit[_key] = 0;
-      radiansPerSecondJoints[_key] = -radiansPerSecondJoints[_key];
-    }
+
     robot.joints[jointKey].setJointValue(jointValue);
   }
-
-  // mesh.rotation.y += radiansPerSecond * delta;
 
   for (let i = 1; i <= 6; i++) {
     mesh = (mesh as URDFRobot);
     _setJointValue(mesh, `HP${i}`, delta, radiansPerSecondJoints["HP"]);
-    // _setJointValue(mesh, `KP${i}`, delta, radiansPerSecondJoints["KP"]);
-    // _setJointValue(mesh, `AP${i}`, delta, radiansPerSecondJoints["AP"]);
+    _setJointValue(mesh, `KP${i}`, delta, radiansPerSecondJoints["KP"]);
   }
 };
-
-const meshes = computed(() => {
-  const amount = 10;
-  const offset = (amount - 1) / 2;
-
-  const materialColorProp = 'rgb(255, 255, 0)';
-  const materialEmissiveColor = 'rgb(255,128,64)';
-  const materialSpecularColor = 'rgb(255,155,255)';
-  const materialShininess = 10;
-  const materialOpacity = 1;
-
-  let arr = [];
-  for (let i = 0; i < amount; i++) {
-    for (let j = 0; j < amount; j++) {
-      for (let k = 0; k < amount; k++) {
-        const object = {
-          key: `${i}${j}${k}`,
-          geometry: 'Capsule',
-          material: 'Phong',
-          position: { x: offset - i, y: offset - j, z: offset - k },
-          materialProps: {
-            color: materialColorProp,
-            emissive: materialEmissiveColor,
-            specular: materialSpecularColor,
-            shininess: materialShininess,
-            opacity: materialOpacity
-          },
-          geometryProps: {
-            radius: 0.3,
-            length: 0.3
-          }
-        };
-        arr.push(object);
-      }
-    }
-  }
-
-  return arr;
-});
 
 </script>
 
